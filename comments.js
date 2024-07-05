e41596c78357e033e440cb7703b7baee5435a331
dnoiro <159739645+dnoiro@users.noreply.github.com>
2024-07-05 05:44:20 +0000

    Copilot third commit

--- comments.js
+++ comments.js
@@ -1,22 +1,38 @@
 // create a web server
-const express = require('express');
-const app = express();
-const port = 3000;
-// use the express middleware to parse the request body
-app.use(express.json());
-// create a global array to store the comments
-let comments = [];
-// create a GET route for the comments
-app.get('/comments', (req, res) => {
-  res.json(comments);
+var http = require('http');
+var fs = require('fs');
+var url = require('url');
+var path = require('path');
+var comments = [];
+var server = http.createServer(function (req, res) {
+    var parseUrl = url.parse(req.url, true);
+    var pathName = parseUrl.pathname;
+    if (pathName === '/') {
+        fs.readFile('./index.html', function (err, data) {
+            if (err) {
+                res.writeHead(404, { 'Content-Type': 'text/html' });
+                res.end('404 Not Found');
+            }
+            res.writeHead(200, { 'Content-Type': 'text/html' });
+            res.end(data);
+        });
+    } else if (pathName === '/submit') {
+        var comment = parseUrl.query;
+        comments.push(comment);
+        res.writeHead(200, { 'Content-Type': 'text/plain' });
+        res.end(JSON.stringify(comments));
+    } else {
+        var filePath = path.join(__dirname, pathName);
+        fs.readFile(filePath, function (err, data) {
+            if (err) {
+                res.writeHead(404, { 'Content-Type': 'text/html' });
+                res.end('404 Not Found');
+            }
+            res.writeHead(200);
+            res.end(data);
+        });
+    }
 });
-// create a POST route for the comments
-app.post('/comments', (req, res) => {
-  const comment = req.body;
-  comments.push(comment);
-  res.status(201).send();
-});
-// start the server
-app.listen(port, () => {
-  console.log(`Server is running on port ${port}`);
-});
+
+server.listen(3000, function () {
+    console.log('Server is running at http://
